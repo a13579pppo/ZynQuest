6edcaec4484d62e7604c81b055c2d2d85d6ebf03
a13579pppo <a91919191918182837@gmail.com>
2025-05-19 00:43:45 +0330

    Update nfts.js

--- frontend/src/pagsrc/data/nfts.js
+++ frontend/src/pagsrc/data/nfts.js
@@ -1,4 +1,46 @@
-const nftData = [ { id: 1, name: "Crystal Fox", category: "Fantasy", priceTON: 0.5, image: "/nfts/fantasy1.png", levelRequired: 1, }, { id: 2, name: "Cyber Samurai", category: "Action", priceTON: 1.5, image: "/nfts/action1.png", levelRequired: 2, }, { id: 3, name: "Royal Elf", category: "Classic", priceTON: 2, image: "/nfts/classic1.png", levelRequired: 3, }, { id: 4, name: "Pixel Dragon", category: "Fantasy", priceTON: 3.5, image: "/nfts/fantasy2.png", levelRequired: 5, }, { id: 5, name: "Mecha Gorilla", category: "Action", priceTON: 5, image: "/nfts/action2.png", levelRequired: 7, } ];
+// مسیر: src/data/nfts.js
+
+const nftData = [
+  {
+    id: 1,
+    name: "Crystal Fox",
+    category: "Fantasy",
+    priceTON: 0.5,
+    image: "/nfts/fantasy1.png",
+    levelRequired: 1,
+  },
+  {
+    id: 2,
+    name: "Cyber Samurai",
+    category: "Action",
+    priceTON: 1.5,
+    image: "/nfts/action1.png",
+    levelRequired: 2,
+  },
+  {
+    id: 3,
+    name: "Royal Elf",
+    category: "Classic",
+    priceTON: 2,
+    image: "/nfts/classic1.png",
+    levelRequired: 3,
+  },
+  {
+    id: 4,
+    name: "Pixel Dragon",
+    category: "Fantasy",
+    priceTON: 3.5,
+    image: "/nfts/fantasy2.png",
+    levelRequired: 5,
+  },
+  {
+    id: 5,
+    name: "Mecha Gorilla",
+    category: "Action",
+    priceTON: 5,
+    image: "/nfts/action2.png",
+    levelRequired: 7,
+  },
+];
 
 export default nftData;
-
